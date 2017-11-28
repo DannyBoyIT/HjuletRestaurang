cf063caf1451ecc919943582ae99e6a2f07e29a7
DannyBoy <d_iliovits@windowslive.com>
2017-11-28 13:43:01 +0100

    Restaurant hubs send order now splits the stirng and creates an order. It sends the order to the client side kitchen.

--- HjuletRestaurang/Hubs/RestaurantHub.cs
+++ HjuletRestaurang/Hubs/RestaurantHub.cs
@@ -1,21 +1,38 @@
 ﻿using System;
+using System.Linq;
 using HjuletRestaurang.Data;
+using HjuletRestaurang.Models;
 using Microsoft.AspNet.SignalR;
 
 namespace HjuletRestaurang.Hubs
 {
     public class RestaurantHub : Hub
     {
-        private ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+
         public RestaurantHub()
         {
             _context = ApplicationDbContext.GetInstance;
         }
+
+        //Gets checked dishes from the customers clientside and creates an order. Sends the order to the kitchen client side.
         public void SendOrder(string message)
         {
-            //todo split message, save to context, send to Kitchen Index
-            //var guid = Guid.Parse(message);
-            Clients.All.sendOrder(message);
+            var order = new Order();
+
+            var dishGuids = message.Split(',');
+
+            foreach (var guid in dishGuids)
+            {
+                var dish = _context.Dishes.FirstOrDefault(d => d.Id == Guid.Parse(guid));
+
+                if (dish != null)
+                    order.Dishes.Add(dish);
+            }
+
+            _context.Orders.Add(order);
+
+            Clients.All.sendOrder(order);
         }
     }
 }
