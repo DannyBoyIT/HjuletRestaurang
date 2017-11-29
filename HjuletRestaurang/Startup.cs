882b332fcf5ea1bcddc75c037f1dbee98941ad68
Tanja Svärd <tanja_jazz@hotmail.com>
2017-11-29 14:25:51 +0100

    added disabled class to shopping cart

--- HjuletRestaurang/Startup.cs
+++ HjuletRestaurang/Startup.cs
@@ -19,7 +19,7 @@ namespace HjuletRestaurang
             var context = ApplicationDbContext.GetInstance;
             var dish1 = new Dish {Id = Guid.NewGuid(), Name = "Pasta Contono", Price = 89};
             var dish2 = new Dish { Id = Guid.NewGuid(), Name = "Fläskkorv med mos", Price = 59 };
-            var dish3 = new Dish { Id = Guid.NewGuid(), Name = "LyxKalops", Price = 189 };
+            var dish3 = new Dish { Id = Guid.NewGuid(), Name = "Lyxkalops", Price = 189 };
             context.Dishes.Add(dish1);
             context.Dishes.Add(dish2);
             context.Dishes.Add(dish3);
