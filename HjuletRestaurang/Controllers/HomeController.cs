562f5f5768e72913798000005666afd8ea55f8f8
DannyBoy <d_iliovits@windowslive.com>
2017-11-30 13:48:28 +0100

    Home/Index now sends the dish guids via the controller and the hub to the clients. Home/Orders now updates with the latest order.

--- HjuletRestaurang/Controllers/HomeController.cs
+++ HjuletRestaurang/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using HjuletRestaurang.Data;
+using HjuletRestaurang.Hubs;
+using HjuletRestaurang.Models;
+using Microsoft.AspNet.SignalR;
 
 namespace HjuletRestaurang.Controllers
 {
@@ -21,9 +23,35 @@ namespace HjuletRestaurang.Controllers
             return View(_context.Dishes);
         }
 
+        [HttpPost]
+        public ActionResult Index(List<Guid> dishIds)
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<RestaurantHub>();
+
+            var order = new Order()
+            {
+                Id = Guid.NewGuid(),
+                Dishes = new List<Dish>(),
+                OrderNumber = _context.Orders.Count != 0 ? _context.Orders.OrderByDescending(o => o.OrderNumber).First().OrderNumber + 1 : 1
+            };
+
+            foreach (var guid in dishIds)
+            {
+                var dish = _context.Dishes.FirstOrDefault(d => d.Id == guid);
+
+                if (dish != null)
+                    order.Dishes.Add(dish);
+            }
+
+            _context.Orders.Add(order);
+
+            hubContext.Clients.All.getOrder(order);
+
+            return View("Orders", _context.Orders.OrderByDescending(x => x.OrderNumber).ToList());
+        }
+
         public ActionResult Orders()
         {
-
             return View(_context.Orders.OrderByDescending(o=>o.OrderNumber).ToList());
         }
     }
--- HjuletRestaurang/Hubs/RestaurantHub.cs
+++ HjuletRestaurang/Hubs/RestaurantHub.cs
@@ -17,35 +17,38 @@ namespace HjuletRestaurang.Hubs
         }
 
         //Gets checked dishes from the customers clientside and creates an order. Sends the order to the kitchen client side.
-        public void SendOrder(string message)
-        {
-            var order = new Order()
-            {
-                Id = Guid.NewGuid(),
-                Dishes = new List<Dish>(),
-                OrderNumber = _context.Orders.Count != 0 ? _context.Orders.OrderByDescending(o => o.OrderNumber).First().OrderNumber + 1 : 1
-            };
+        //public void SendOrder(string message)
+        //{
+        //    var order = new Order()
+        //    {
+        //        Id = Guid.NewGuid(),
+        //        Dishes = new List<Dish>(),
+        //        OrderNumber = _context.Orders.Count != 0 ? _context.Orders.OrderByDescending(o => o.OrderNumber).First().OrderNumber + 1 : 1
+        //    };
 
-            var dishGuids = message.Split(',');
+        //    var dishGuids = message.Split(',');
 
-            foreach (var guid in dishGuids)
-            {
-                var dish = _context.Dishes.FirstOrDefault(d => d.Id == Guid.Parse(guid));
+        //    foreach (var guid in dishGuids)
+        //    {
+        //        var dish = _context.Dishes.FirstOrDefault(d => d.Id == Guid.Parse(guid));
 
-                if (dish != null)
-                    order.Dishes.Add(dish);
-            }
+        //        if (dish != null)
+        //            order.Dishes.Add(dish);
+        //    }
 
-            _context.Orders.Add(order);
-
-            Clients.All.getOrder(order);
-        }
+        //    _context.Orders.Add(order);
+            
+        //    Clients.All.getOrder(order);
+        //}
 
         public void OrderReady(string orderId)
         {
             var order = _context.Orders.FirstOrDefault(o => o.Id == Guid.Parse(orderId));
+
+            if (order != null)
             order.IsReady = true;
-            Clients.All.orderReady();
+
+            Clients.All.orderReady(order);
         }
     }
 }
